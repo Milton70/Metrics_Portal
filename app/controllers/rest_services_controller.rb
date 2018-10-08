4cde21dab36509454669740808343d3969d975f8
Mik Bartram <milton@poetic.com>
2018-10-08 21:08:27 +0100

    created page to show rest get, post, put and delete actions

--- app/controllers/rest_services_controller.rb
+++ app/controllers/rest_services_controller.rb
@@ -4,14 +4,28 @@ class RestServicesController < ApplicationController
 		@json_response = "{ this is where some json data will be displayed }"
 	end
 
-	def new
+	def show
 		@json_response = RestService.get_all_employees
 		render 'index'		
 	end
 
-	def edit
+	def get_an_employee
 		@json_response = RestService.get_an_employee(params[:id])
 		render 'index'
 	end
 
+	def edit
+		@json_response = RestService.update_an_employee(params[:id], params[:name], params[:salary], params[:age])
+		render 'index'
+	end
+
+	def destroy
+		@json_response = RestService.delete_an_employee(params[:id])
+		render 'index'
+	end
+
+	def new
+		@json_response = RestService.create_an_employee(params[:id])
+		render 'index'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'sessions#index' 
 
   resources :users, :roles, :sessions, :rest_services
+
+  get '/rest_services/get_employee/:id', 	to: 'rest_services#get_an_employee', 	as: 'get_an_employee'
+
 end
